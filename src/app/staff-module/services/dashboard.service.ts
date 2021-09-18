cc0b0d07c7cbe91d1cff63ec5f28ca67f60a09b3
shahid-aim <shahid@aimbreakthrough.com>
2021-09-18 15:45:01 +0530

    Resolved Conflicts
    1 > Donor not getting selected
    2 > Added Logout

--- src/app/staff-module/services/dashboard.service.ts
+++ src/app/staff-module/services/dashboard.service.ts
@@ -4,57 +4,63 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ExternalUrlMappings } from '../shared/UrlMapping';
 import { CreatePasturization, CreatePool, CreateRawCollection, TestResult } from '../models/phase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
-  token : any
-  constructor(private _httpClient : HttpClient) {
+  token: any
+  constructor(private _httpClient: HttpClient, private _router : Router) {
     this.token = localStorage.getItem("token")
-   }
-
-  getStaff(): Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_STAFF, {headers : new HttpHeaders().set("Authorization", this.token)})
-  }  
-
-  getLatestDonorAppointment() : Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_LATEST_DONOR_APPOINTMENT, {headers : new HttpHeaders().set("Authorization", this.token)})
   }
 
-  getRawCollection(): Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_RAW_COLLECTION, {headers : new HttpHeaders().set("Authorization", this.token)})
+  logoutUser(){
+    localStorage.removeItem("token")
+    this._router.navigateByUrl("/login")
   }
 
-  createRawCollection(createRawCollection : CreateRawCollection) : Observable<any>{
-    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_RAW_COLLECTION, createRawCollection, {headers : new HttpHeaders().set("Authorization", this.token)})
+  getStaff(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_STAFF, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
-  getDonorInfo(): Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_DONOR_INFO, {headers : new HttpHeaders().set("Authorization", this.token)})
+  getLatestDonorAppointment(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_LATEST_DONOR_APPOINTMENT, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
-
-  getPool() : Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_POOL, {headers : new HttpHeaders().set("Authorization", this.token)})
+  getRawCollection(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_RAW_COLLECTION, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
-  createPool(createPool : CreatePool) : Observable<any>{
-    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_POOL, createPool, {headers : new HttpHeaders().set("Authorization", this.token)})
+  createRawCollection(createRawCollection: CreateRawCollection): Observable<any> {
+    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_RAW_COLLECTION, createRawCollection, { headers: new HttpHeaders().set("Authorization", this.token) })
+  }
+
+  getDonorInfo(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_DONOR_INFO, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
 
-  setTestResult(testResult : TestResult) : Observable<any>{
-    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.SET_TEST_RESULT, testResult, {headers : new HttpHeaders().set("Authorization", this.token)})
+  getPool(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_POOL, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
-  createPasturization(createPasturization : CreatePasturization) : Observable<any> {
-    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_PASTURIZATION, createPasturization, {headers : new HttpHeaders().set("Authorization", this.token)})
+  createPool(createPool: CreatePool): Observable<any> {
+    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_POOL, createPool, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
 
-  getPasturization(): Observable<any>{
-    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_PASTURIZATION, {headers : new HttpHeaders().set("Authorization", this.token)})
+  setTestResult(testResult: TestResult): Observable<any> {
+    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.SET_TEST_RESULT, testResult, { headers: new HttpHeaders().set("Authorization", this.token) })
+  }
+
+  createPasturization(createPasturization: CreatePasturization): Observable<any> {
+    return this._httpClient.post(environment.BASE_URL + ExternalUrlMappings.CREATE_PASTURIZATION, createPasturization, { headers: new HttpHeaders().set("Authorization", this.token) })
+  }
+
+
+  getPasturization(): Observable<any> {
+    return this._httpClient.get(environment.BASE_URL + ExternalUrlMappings.GET_PASTURIZATION, { headers: new HttpHeaders().set("Authorization", this.token) })
   }
 
 }
--- src/app/staff-module/staff-module.component.ts
+++ src/app/staff-module/staff-module.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DashboardService } from './services/dashboard.service';
+import { InternalUrlMappings } from './shared/UrlMapping';
 
 @Component({
   selector: 'app-staff-module',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffModuleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _dashboardService : DashboardService, private _router  : Router) { }
 
   ngOnInit(): void {
   }
@@ -16,6 +19,20 @@ export class StaffModuleComponent implements OnInit {
     document.getElementById("mySidenav").style.width = "250px"
   }
 
+  routeAndClose(routeTo : string){
+    if(routeTo == "dashboard")
+      this._router.navigateByUrl("staff/" + InternalUrlMappings.DASHBOARD)
+    else if(routeTo == "raw-collection")
+      this._router.navigateByUrl("staff/" + InternalUrlMappings.RAW_COLLECTION)
+    else if(routeTo == "pooling")
+      this._router.navigateByUrl("staff/" + InternalUrlMappings.POOLING)
+    else if(routeTo == "pasturization")
+      this._router.navigateByUrl("staff/" + InternalUrlMappings.PASTURIZATION)
+    else if(routeTo == "logout"){
+      this._dashboardService.logoutUser()
+    }
+    this.closeNav()
+    }
   
   closeNav(){
     document.getElementById("mySidenav").style.width = "0px"
