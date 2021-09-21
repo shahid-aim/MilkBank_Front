5268d12938893bc5c9822402c31d7af5bd11d861
mayurikoli26 <88818168+mayurikoli26@users.noreply.github.com>
2021-09-21 16:25:46 +0530

    refresh pooling , raw-collection and pasturization
    refresh pooling , raw-collection and pasturization

--- src/app/donor-view/donor-profile/donor-profile.component.ts
+++ src/app/donor-view/donor-profile/donor-profile.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonorProfileComponent implements OnInit {
 
+  fullName : string = ""
+  contact : number;
+  email_id:string= "";
+  dob:string;
+  address:string= "";
+  city:string= "";
+  taluka:string= "";
+  district:string= '';
   constructor() { }
 
   ngOnInit(): void {
+    this.fullName = "Hello World"
+    this.contact = 7789787890;
+    this.email_id = "abc@gmail.com";
+    this.dob = "25/3/1998" ;
+    this.address= "Pune";
+    this.city= "Pune";
+    this.taluka= "Haveli";
+    this.district= "Pune"
   }
 
 }
