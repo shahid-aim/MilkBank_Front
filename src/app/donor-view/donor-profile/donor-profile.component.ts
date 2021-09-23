8fdedebb4327c662523f4c72e89e1e26872adbf6
mayurikoli26 <88818168+mayurikoli26@users.noreply.github.com>
2021-09-23 11:13:45 +0530

    refresh donor-profile
    refresh donor-profile

--- src/app/donor-view/donor-profile/donor-profile.component.ts
+++ src/app/donor-view/donor-profile/donor-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DonorRegistrationService } from 'src/app/service/donor-registration/donor-registration.service';
+import { InternalUrlMappings } from 'src/app/shared/UrlMapping';
 
 @Component({
   selector: 'app-donor-profile',
@@ -10,47 +12,59 @@ export class DonorProfileComponent implements OnInit {
 
   token: string
 
-  fullName: string = ""
+  fullName: string = "";
   contact: number;
   email_id: string = "";
   dob: string;
   address: string = "";
   city: string = "";
   taluka: string = "";
-  district: string = '';
+  district: string = "";
 
-  yes: boolean = true;
-  no: boolean = false;
-  constructor(private _donorService: DonorRegistrationService) { }
+  acute_disease: string
+  fever_or_rashes: string
+  hiv_hbag_disease: string
+  blood_transfusion: string
+  nicotine_alchol_consumption: string
+
+
+  constructor(private _donorService: DonorRegistrationService, private _router : Router) { }
 
   ngOnInit(): void {
-
     this.token = localStorage.getItem("token")
     this.getDonorProfile()
-
-
-    this.yes;
-    this.no;
   }
 
   // API
   getDonorProfile() {
     this._donorService.getDonorProfile(this.token).subscribe(response => {
-      console.log(response)
-    response = response.donor
-      
-    this.fullName = response.full_name
-    this.contact = response.phone_number
-    this.email_id = response.email
-    this.dob = response.DOD     // Change this to DOD in future
-    this.address = response.address
-    // this.city = response.
-    // this.taluka = response.
-    // this.district = response.
-    }
-    
-    
-    )
+      this.fullName = response.full_name;
+      this.contact = response.phone_number
+      this.email_id = response.email
+      this.dob = response.DOD
+      this.address = response.address
+      this.city = response.city
+      this.taluka = response.taluka
+      this.district = response.district
+
+      this.acute_disease = (response.acute_disease == "false") ? "Yes" : "No"
+      this.fever_or_rashes = (response.fever_or_rashes == "false") ? "Yes" : "No"
+      this.hiv_hbag_disease = (response.hiv_hbag_disease == "false") ? "Yes" : "No"
+      this.blood_transfusion = (response.blood_transfusion == "false") ? "Yes" : "No"
+      this.nicotine_alchol_consumption = (response.nicotine_alchol_consumption == "false") ? "Yes" : "No"
+    },
+    error => {
+      if (error.status == 401) {
+        localStorage.removeItem("token")
+        this._router.navigateByUrl(InternalUrlMappings.LOGIN)
+      }
+    })
   }
 
 }
+
+
+// background-color: white;
+// padding: 20px;
+// border-radius: 10px;
+// box-shadow: 2px 2px 2px rgb(0 0 0 / 25%);
